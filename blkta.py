888726544e46ee651fd3f708cae86433db6dfc38
pplau <pplauu@qq.com>
2017-11-10 21:26:32 +0800

    fix a bug

--- blkta.py
+++ blkta.py
@@ -18,30 +18,32 @@ class Analysis:
 		self.write_res = {}
 
 	def __IO_statistc__(self):
-		self.read_res['perc_4k'] = round(float(self.read_count['io_4k']) / float(self.total_count['read_total']), 2)
-		self.read_res['perc_4kto8k'] = round(float(self.read_count['io_4kto8k']) / float(self.total_count['read_total']), 2)
-		self.read_res['perc_8kto16k'] = round(float(self.read_count['io_8kto16k']) / float(self.total_count['read_total']), 2)
-		self.read_res['perc_16kto32k'] = round(float(self.read_count['io_16kto32k']) / float(self.total_count['read_total']), 2)
-		self.read_res['perc_32kto64k'] = round(float(self.read_count['io_32kto64k']) / float(self.total_count['read_total']), 2)
-		self.read_res['perc_big'] = round(float(self.read_count['io_big']) / float(self.total_count['read_total']), 2)
+		if self.total_count['read_total'] != 0:
+			self.read_res['perc_4k'] = round(float(self.read_count['io_4k']) / float(self.total_count['read_total']), 2)
+			self.read_res['perc_4kto8k'] = round(float(self.read_count['io_4kto8k']) / float(self.total_count['read_total']), 2)
+			self.read_res['perc_8kto16k'] = round(float(self.read_count['io_8kto16k']) / float(self.total_count['read_total']), 2)
+			self.read_res['perc_16kto32k'] = round(float(self.read_count['io_16kto32k']) / float(self.total_count['read_total']), 2)
+			self.read_res['perc_32kto64k'] = round(float(self.read_count['io_32kto64k']) / float(self.total_count['read_total']), 2)
+			self.read_res['perc_big'] = round(float(self.read_count['io_big']) / float(self.total_count['read_total']), 2)
 
-		self.write_res['perc_4k'] = round(float(self.write_count['io_4k']) / float(self.total_count['write_total']), 2)
-		self.write_res['perc_4kto8k'] = round(float(self.write_count['io_4kto8k']) / float(self.total_count['write_total']), 2)
-		self.write_res['perc_8kto16k'] = round(float(self.write_count['io_8kto16k']) / float(self.total_count['write_total']), 2)
-		self.write_res['perc_16kto32k'] = round(float(self.write_count['io_16kto32k']) / float(self.total_count['write_total']), 2)
-		self.write_res['perc_32kto64k'] = round(float(self.write_count['io_32kto64k']) / float(self.total_count['write_total']), 2)
-		self.write_res['perc_big'] = round(float(self.write_count['io_big']) / float(self.total_count['write_total']), 2)
+		if self.total_count['write_total'] != 0:
+			self.write_res['perc_4k'] = round(float(self.write_count['io_4k']) / float(self.total_count['write_total']), 2)
+			self.write_res['perc_4kto8k'] = round(float(self.write_count['io_4kto8k']) / float(self.total_count['write_total']), 2)
+			self.write_res['perc_8kto16k'] = round(float(self.write_count['io_8kto16k']) / float(self.total_count['write_total']), 2)
+			self.write_res['perc_16kto32k'] = round(float(self.write_count['io_16kto32k']) / float(self.total_count['write_total']), 2)
+			self.write_res['perc_32kto64k'] = round(float(self.write_count['io_32kto64k']) / float(self.total_count['write_total']), 2)
+			self.write_res['perc_big'] = round(float(self.write_count['io_big']) / float(self.total_count['write_total']), 2)
 
 	def run(self):
 		for line in self.blk_file.readlines():
 			value_list = re.split(r'\s+', line)
 
-			if value_list[5] == 'C':
+			if value_list[6] == 'C':
 				# success write/read to/from  disk
-				if value_list[6] == 'W' or value_list[6] == 'WM':
+				if value_list[7] == 'W' or value_list[7] == 'WM':
 					# write to disk
 					self.total_count['write_total'] +=1
-					io_size = int(value_list[9])
+					io_size = int(value_list[10])
 					if io_size <= 8:
 						self.write_count['io_4k'] +=1
 
@@ -60,10 +62,10 @@ class Analysis:
 					if io_size > 128:
 						self.write_count['io_big'] +=1
 
-				if value_list[6] == 'R' or value_list[6] == 'RM':
+				if value_list[7] == 'R' or value_list[7] == 'RM':
 					# read from disk
 					self.total_count['read_total'] +=1
-					io_size = int(value_list[9])
+					io_size = int(value_list[10])
 					if io_size <= 8:
 						self.read_count['io_4k'] +=1
 
@@ -85,6 +87,7 @@ class Analysis:
 		self.__IO_statistc__()
 		print self.read_res
 		print self.write_res
+
 
 
 
